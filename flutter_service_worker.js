3a832de2fa6361bbbac872b75b7f842d5980d8f8
malinkang <malinkang@ushareit.com>
2021-07-16 14:41:51 +0800

    first commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,15 +4,15 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3392e9ab91f3aa3d6a06beeb59f24a1c",
-"index.html": "63ebe6c108a473e1c222556e66ab9883",
-"/": "63ebe6c108a473e1c222556e66ab9883",
-"main.dart.js": "6e6187b938e96fa0e77c6689a9df248d",
+"index.html": "bd23ba86dbdcf772ee6611868eba5d7f",
+"/": "bd23ba86dbdcf772ee6611868eba5d7f",
+"main.dart.js": "535434c1f030349979eca8a70c94361e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "74b1d0be9157eb3a6f299ef23677887d",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "6aeca6bf2ffc2e73993faa4b70f5b8b6",
+"assets/NOTICES": "bde9ee889e7cdb2c908965b81d635c52",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
